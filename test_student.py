361fff642fff18d579e9927c5c1e122cf29d48b4
s-batish <>
2023-05-26 12:55:27 +0000

    Implement mocking to test requests

--- test_student.py
+++ test_student.py
@@ -23,6 +23,7 @@
 import unittest
 from student import Student
 from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
@@ -68,6 +69,25 @@ class TestStudent(unittest.TestCase):
         self.student.apply_extension(5)
         self.assertEqual(self.student.end_date, self.student._start_date + timedelta(days=370))
         """
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failed(self):
+        """
+        In the path "student" comes from the name of the file student.py
+        If you have named the file something else - it will need to match
+        eg, students.py would need "students.requests.get"
+        """
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request")
 
 
 if __name__ == "__main__":
